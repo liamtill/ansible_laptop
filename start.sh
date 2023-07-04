d6cebb6fdae82d780c50dfc82b5c99deb6fd9dbc
Liam Till <liam.till@live.co.uk>
2023-07-04 20:59:39 +0100

    refactor some tasks to be cleaner. expand go install.

--- start.sh
+++ start.sh
@@ -3,21 +3,22 @@
 # Initial script to install requirements to run Ansible
 
 echo "Installing curl"
-sudo apt install curl
+sudo apt install -y curl
 
 echo "Installing pip and Ansible"
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py --user
+curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+python3 /tmp/get-pip.py --user
 # python3 -m pip install --user ansible
 
-sudo apt install software-properties-common
+sudo apt install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install -y ansible
 echo "Checking Ansible version"
 ansible --version
 
 echo "Running playbook"
-ansible-playbook playbooks/main.yaml
+# ensure ansible knows the sudo password
+ansible-playbook playbooks/main.yaml --ask-become-pass
 
 echo "Final Steps:"
 echo "Configure syncthing"
